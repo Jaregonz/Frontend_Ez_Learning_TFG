829197066276f68eece740383ae7b274cd8c09e3
Jaregonz <jaregonz.jr@gmail.com>
2025-06-02 10:39:18 +0200

    Lista para elegir profesor en el registro

--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../App.css';
-import '../styles/style.css';
-import '../styles/footer.css';
-import '../styles/header.css';
-import Footer from './Footer';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "../App.css";
+import "../styles/style.css";
+import "../styles/footer.css";
+import "../styles/header.css";
+import Footer from "./Footer";
 const RegisterPage = () => {
   const navigate = useNavigate();
+  const [profesores, setProfesores] = useState([]);
   const [formData, setFormData] = useState({
     username: "",
     password: "",
@@ -17,15 +18,30 @@ const RegisterPage = () => {
     nivel: "",
     rol: "ALUMNO",
     imagenPerfil: "",
+    idProfesor: "",
   });
 
   const handleChange = (e) => {
-    const { name, value, type, checked } = e.target;
-    setFormData((prev) => ({
-      ...prev,
-      [name]: type === "checkbox" ? (checked ? "PROFESOR" : "ALUMNO") : value,
-      ...(name === "correoElectronico" && { username: value }),
-    }));
+    const { name, value, checked } = e.target;
+
+    if (name === "rol") {
+      setFormData((prev) => ({
+        ...prev,
+        rol: checked ? "PROFESOR" : "ALUMNO",
+        idProfesor: checked ? "" : prev.idProfesor,
+      }));
+    } else if (name === "correoElectronico") {
+      setFormData((prev) => ({
+        ...prev,
+        correoElectronico: value,
+        username: value,
+      }));
+    } else {
+      setFormData((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    }
   };
 
   const handleSubmit = async (e) => {
@@ -51,6 +67,26 @@ const RegisterPage = () => {
     }
   };
 
+  useEffect(() => {
+    const fetchProfesores = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:8080/usuarios/profesores"
+        );
+        if (response.ok) {
+          const data = await response.json();
+          setProfesores(data);
+        } else {
+          throw new Error("No se pudo cargar la lista de profesores");
+        }
+      } catch (error) {
+        console.error("Error al obtener profesores:", error);
+      }
+    };
+
+    fetchProfesores();
+  }, []);
+
   return (
     <div className="RegisterPage">
       <header>
@@ -66,21 +102,54 @@ const RegisterPage = () => {
           <div className="form-register">
             <h1 className="titulo-register">CREAR CUENTA</h1>
             <form className="register" onSubmit={handleSubmit}>
-
               <label htmlFor="nombre">Nombre</label>
-              <input type="text" id="nombre" name="nombre" placeholder="Nombre" required onChange={handleChange} />
+              <input
+                type="text"
+                id="nombre"
+                name="nombre"
+                placeholder="Nombre"
+                required
+                onChange={handleChange}
+              />
 
               <label htmlFor="apellidos">Apellidos</label>
-              <input type="text" id="apellidos" name="apellidos" placeholder="Apellidos" required onChange={handleChange} />
+              <input
+                type="text"
+                id="apellidos"
+                name="apellidos"
+                placeholder="Apellidos"
+                required
+                onChange={handleChange}
+              />
 
               <label htmlFor="password">Contraseña</label>
-              <input type="password" id="password" name="password" placeholder="Contraseña" required onChange={handleChange} />
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Contraseña"
+                required
+                onChange={handleChange}
+              />
 
               <label htmlFor="correoElectronico">Correo Electrónico</label>
-              <input type="email" id="correoElectronico" name="correoElectronico" placeholder="Correo Electrónico" required onChange={handleChange} />
+              <input
+                type="email"
+                id="correoElectronico"
+                name="correoElectronico"
+                placeholder="Correo Electrónico"
+                required
+                onChange={handleChange}
+              />
 
               <label htmlFor="fechaNacimiento">Fecha de Nacimiento</label>
-              <input type="date" id="fechaNacimiento" name="fechaNacimiento" required onChange={handleChange} />
+              <input
+                type="date"
+                id="fechaNacimiento"
+                name="fechaNacimiento"
+                required
+                onChange={handleChange}
+              />
 
               <label htmlFor="nivel">Nivel</label>
               <select id="nivel" name="nivel" required onChange={handleChange}>
@@ -89,15 +158,43 @@ const RegisterPage = () => {
                 <option value="B2">B2</option>
                 <option value="C1">C1</option>
               </select>
-              <div className="checkbox-group">
-                <label htmlFor="rol">Soy profesor</label>
-                <input type="checkbox" id="rol" name="rol" onChange={handleChange} />
-              </div>
-              
-              <label htmlFor="imagenPerfil">Imagen de Perfil (URL)</label>
-              <input type="text" id="imagenPerfil" name="imagenPerfil" placeholder="URL de la imagen de perfil" onChange={handleChange} />
 
-              <button className="boton" type="submit">REGISTRARSE</button>
+              <label htmlFor="imagenPerfil">Imagen de Perfil (URL)</label>
+              <input
+                type="text"
+                id="imagenPerfil"
+                name="imagenPerfil"
+                placeholder="URL de la imagen de perfil"
+                onChange={handleChange}
+              />
+
+              <div className="checkbox-group">
+                <input
+                  type="checkbox"
+                  id="rol"
+                  name="rol"
+                  onChange={handleChange}
+                />
+                <label htmlFor="rol">Soy profesor</label>
+              </div>
+              <label htmlFor="profesor">Profesor asignado</label>
+              <select
+                id="profesor"
+                name="idProfesor"
+                onChange={handleChange}
+                disabled={formData.rol === "PROFESOR"}
+              >
+                <option value="">Selecciona un profesor</option>
+                {profesores.map((profesor) => (
+                  <option key={profesor.id} value={profesor.id}>
+                    {profesor.nombre} {profesor.apellidos}
+                  </option>
+                ))}
+              </select>
+
+              <button className="boton" type="submit">
+                REGISTRARSE
+              </button>
             </form>
           </div>
 
@@ -108,7 +205,6 @@ const RegisterPage = () => {
         </div>
       </main>
       <Footer />
-      
     </div>
   );
 };
